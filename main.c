6ad0608be6e708115a35d1cff790ccaa342e1eb8
Nikita Nikiforov <nikiforov-2001@mail.ru>
2021-03-04 22:09:04 +0300

    Добавлены проверки

--- main.c
+++ main.c
@@ -6,12 +6,24 @@
 int main(int argc, char** argv) {
 	setlocale(LC_ALL, "Rus");
 
+	if (argc != 4) {
+		printf("Ошибка. Возможные аргументы\n");
+		printf("--pack <Путь исходного файла> <Путь итогового архива>\n");
+		printf("--unpack <Путь архива> <Путь распаковынных файлов>\n");
+		return 1;
+	}
+
 	char* action = argv[1];
 	char* source = argv[2];
+	char* output = argv[3];
 
 	if (access(source, 0) == -1) {
 		printf("Ошибка. %s не существует.\n", source);
+		return 1;
 	}
-	
-	exit(0);
+
+
+
+	printf("Успешно.")
+	return 0;
 }
