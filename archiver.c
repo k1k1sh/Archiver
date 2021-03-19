27fe60b05b6f438075c47f1efe6e21ff782edbd5
Nikita Nikiforov <nikiforov-2001@mail.ru>
2021-03-19 23:15:31 +0300

    Закончено архивирование

--- archiver.c
+++ archiver.c
@@ -52,3 +52,162 @@ void pack(char* dir_path, char* archive_path) {
         exit(1);
     }
 }
+
+void _pack(int archive, char* src_path) {
+    // Открытие директории
+    DIR *dir = opendir(src_path);
+
+    // Если путь к директории - это путь к файлу
+    if (dir == NULL) {
+        printf("Ошибка %s - не директория.\n", src_path);
+
+        // Пытаемся закрыть архив
+        if (close(archive) == -1) {
+            printf("Ошибка. Невозможно закрыть архив.\n");
+        }
+
+        exit(1);
+    }
+
+    // Упаковываем информацию об архиве
+    _pack_info(archive, FOLDER_NAME, src_path);
+
+    // Получаем следующую запись в каталоге
+    struct dirent *entry;
+
+    // Пока существует следующий файл в директории
+    while ((entry = readdir(dir)) != NULL) {
+        // Пропускаем вхождение . и ..
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        // Получем путь директории
+        char file_path[MAX_PATH_LENGTH] = {0};
+        snprintf(file_path, sizeof(file_path), "%s/%s", src_path, entry->d_name);
+        _remove_extra_slash(file_path);
+
+        if (entry->d_type == DT_DIR) {
+            // Если это папка
+            _pack(archive, file_path);
+        }
+        else {
+            // Записываем информацию
+            _pack_info(archive, FILE_NAME, file_path);
+
+            // Записываем содержимое
+            _pack_content(archive, file_path);
+
+            // Эхо печать в консоль
+            printf("Записно в архив %s\n", file_path);
+        }
+    }
+
+    // Закрываем директорию
+    if (closedir(dir) == -1) {
+        printf("Ошибка. Невозможно закрыть %s.\n", src_path);
+        exit(1);
+    }
+}
+
+void _pack_info(int fd, path_type p_type, char* path) {
+
+    // Флаг ошибки
+    int error = 1;
+
+    do {
+
+
+        // Записываем длину пути
+        int file_path_len = strlen(path) + 1;
+        if (write(fd, &file_path_len, sizeof(int)) != sizeof(int)) {
+            printf("Ошибка. Невозможно записать длину пути в архив.\n");
+            break;
+        }
+
+        // Записываем путь файла
+        if (write(fd, path, file_path_len) != file_path_len) {
+            printf("Ошибка. Невозможно записать путь в архив.\n");
+            break;
+        }
+
+        error = 0;
+    } while (0);
+
+    if (error) {
+        if (close(fd) == -1) {
+            printf("Ошибка. Невозможно закрыть архив.\n");
+        }
+        exit(1);
+    }
+}
+
+void _pack_content(int fd, char* path) {
+    int error = 1;      // Флаг ошибка
+    int file;           // Файловый дискриптор
+    off_t size;         // Размер получаемого содержимого файла
+    int file_size;      // размер содержимого файла
+    char content_byte;  // Байт для копирования в архив
+
+    // Открываем файл
+    file = open(path, O_RDONLY, S_IRUSR);
+    if (file == -1) {
+        printf("Ошибка. Невозможно открыть %s для чтения.\n", path);
+        close(fd);
+        exit(1);
+    }
+
+    do {
+        // Получаем размер файлв
+        size = lseek(file, 0, SEEK_END);
+        if (size == -1) {
+            printf("Ошибка. Невозможно получить %s длину.\n", path);
+            break;
+        }
+        file_size = (int)(size);
+
+        // Переводим на начало
+        if (lseek(file, 0, SEEK_SET) == -1) {
+            printf("Ошибка. Невозможно поставить курсор %s в начало.\n", path);
+            break;
+        }
+
+        // Записываем размер содержимого
+        if (write(fd, &file_size, u64sz) != u64sz) {
+            printf("Ошибка. Невозможно записать размер %s в архив.\n", path);
+            break;
+        }
+
+        // Чтение содержимого
+        for (int num_bytes = 0; num_bytes < file_size; num_bytes++) {
+            if (read(file, &content_byte, 1) != 1) {
+                printf("Ошибка. Невозможно прочитать содержимое %s.\n", path);
+                break;
+            }
+
+            // Записывем содержимое
+            if (write(fd, &content_byte, 1) != 1) {
+                printf("Ошибка. Невозможно записать модержимое %s в архив.\n", path);
+                break;
+            }
+        }
+
+        error = 0;
+    } while (0);
+
+    // Закрываем архив
+    if (error && close(fd) == -1) {
+        printf("Ошибка. Невозможно закрыть архив.\n");
+    }
+
+    // Закрываем исходный файл
+    if (close(file) == -1) {
+        printf("Ошибка. Невозможно закрыть %s.\n", path);
+        exit(1);
+    }
+
+    // Выход по ошибке
+    if (error) {
+        exit(1);
+    }
+}
--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 #include <locale.h>
 #include <unistd.h>
 
+#include "archiver.h"
+
 int main(int argc, char** argv) {
 	setlocale(LC_ALL, "Rus");
-
+    // Проверка на количество аргументов
 	if (argc != 4) {
 		printf("Ошибка. Возможные аргументы\n");
 		printf("--pack <Путь исходного файла> <Путь итогового архива>\n");
@@ -17,11 +19,18 @@ int main(int argc, char** argv) {
 	char* source = argv[2];
 	char* output = argv[3];
 
+	// Проверка пути к директории
 	if (access(source, 0) == -1) {
 		printf("Ошибка. %s не существует.\n", source);
 		return 1;
 	}
 
+    // Упаковка или распаковка арзива
+    if (strcmp(action, "--pack") == 0) {
+        pack(source, output);
+    } else {
+        unpack(source, output);
+    }
 
 
 	printf("Успешно.")
